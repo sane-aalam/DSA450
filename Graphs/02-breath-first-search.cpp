838cb455c87ece36549c310706e1c1a6a037e7d8
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-02-04 13:33:30 +0530

    Update 02-breath-first-search.cpp

--- Graphs/02-breath-first-search.cpp
+++ Graphs/02-breath-first-search.cpp
@@ -1,15 +1,15 @@
-
-/* Breadth First Traversal 
-
- - ( gfg )
- Given a directed graph.The task is to do Breadth First Traversal of this graph starting from 0. Note : One can move from node u to node v only if there's an edge from u to v and find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration
-
+/* Breadth First Traversal -
+ Given a directed graph.The task is to do Breadth First Traversal of this graph starting from 0. Note : 
+ One can move from node u to node v only if there's an edge from u to v and find the BFS traversal of 
+ the graph starting from the 0th vertex, from left to right according to the graph. Also, 
+ you should only take nodes directly or indirectly connected from Node 0 in consideration
  */
 
-//{ Driver Code Starts
+// TC = o(n) + o(2E)
+// SC = o(n)
+
 #include <bits/stdc++.h>
 using namespace std;
-
 
 // start code here 
 class Solution {
@@ -55,10 +55,7 @@ int main() {
             int u, v;
             cin >> u >> v;
             adj[u].push_back(v);
-            // 		adj[v].push_back(u);
         }
-        // string s1;
-        // cin>>s1;
         Solution obj;
         vector<int> ans = obj.bfsOfGraph(V, adj);
         for (int i = 0; i < ans.size(); i++) {
@@ -68,4 +65,3 @@ int main() {
     }
     return 0;
 }
-// } Driver Code Ends
