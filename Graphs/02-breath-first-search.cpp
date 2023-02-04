2f89c62d555aaf4b31797972d3392266a748c3ab
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-02-04 13:29:31 +0530

    Add files via upload

---
+++ Graphs/02-breath-first-search.cpp
@@ -0,0 +1,71 @@
+
+/* Breadth First Traversal 
+
+ - ( gfg )
+ Given a directed graph.The task is to do Breadth First Traversal of this graph starting from 0. Note : One can move from node u to node v only if there's an edge from u to v and find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration
+
+ */
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// start code here 
+class Solution {
+  public:
+    // Function to return Breadth First Traversal of given graph
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // create visted array and queue data structure for perform BFS algo
+        int vist[V] = {0};
+        vist[0] = 1; 
+        queue<int>q;
+        q.push(0);
+        
+        // create answer array for storing the answer
+        vector<int> result;
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            
+            result.push_back(node);
+            
+            for(auto x : adj[node]){
+                if(!vist[x]){
+                    vist[x] = 1;
+                    q.push(x);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// main function 
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        // create adj list for storing the nieghbours nodes
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            // 		adj[v].push_back(u);
+        }
+        // string s1;
+        // cin>>s1;
+        Solution obj;
+        vector<int> ans = obj.bfsOfGraph(V, adj);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
