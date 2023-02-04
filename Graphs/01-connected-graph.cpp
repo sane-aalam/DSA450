fbc33b714a5d2ef12d4507950d9f4ad6c1394054
Sane Aalam <123074523+sane-aalam@users.noreply.github.com>
2023-02-04 13:31:14 +0530

    Update 01-connected-graph.cpp

---
+++ Graphs/01-connected-graph.cpp
@@ -0,0 +1,11 @@
+// you are give 4 graphs but these all are components of single graphs
+
+// you are give 4 graphs but these all are components of single graphs
+
+int v = 14;
+visted array =>  int visted[V] = {0};
+for(int i=0; i<V; i++){
+    if(!visted[i]){ // if node is not visited 
+        // apply traversal algorithm (bfs and dfs )
+    }
+}
